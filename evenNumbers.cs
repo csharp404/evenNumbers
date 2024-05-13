925b14a49f7b2fe0ac30d824682a621dc212e5c6
csharp404 <yousefjobsdeve@gmail.com>
2024-05-13 18:15:30 +0300

    Add files via upload

---
+++ evenNumbers.cs
@@ -0,0 +1,24 @@
+﻿// Yousef
+// [05/11/2024]
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int i = 20;
+        while(i>0) {
+            if ((i & 1)==0)
+            {
+                Console.WriteLine(i);
+            }
+            i--;
+        }
+        //other solution
+        int j = 20;
+        while (j > 0)
+        {
+                Console.WriteLine(j);
+            
+            j-=2;
+        }
+    }
+}
